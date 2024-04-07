d92ff039eb1685f2136e57db2d5ff7ad4d3f4b1c
Vinicius Dutra <dutra_morais@hotmail.com>
2024-04-07 20:53:55 -0300

    start

--- aula6/k8s/start.sh
+++ aula6/k8s/start.sh
@@ -1,2 +1,23 @@
 #!bin/sh
 minikube start --driver docker --ports=8080:80,30082:30082,31150:31150 --extra-config=kubelet.housekeeping-interval=10s --kubernetes-version=v1.29
+
+
+sleep 15
+
+kubectl apply -f /mysql-master
+sleep 5
+kubectl apply -f /mysql-replica
+sleep 5
+kubectl apply -f /redis
+sleep 5
+kubectl apply -f /job-migration
+sleep 5
+kubectl apply -f /api
+sleep 5
+kubectl apply -f /frontend
+sleep 5
+kubectl apply -f /consumer
+sleep 5
+kubectl apply -f /job-csv
+
+
